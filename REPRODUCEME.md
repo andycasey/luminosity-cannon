ed706c6e1f166c5e9dc84bc805e1218a59c1de7a
Andy Casey <andycasey@gmail.com>
2015-08-11 16:50:26 +0100

    Updated APOGEE script to get aspCap stuff

--- REPRODUCEME.md
+++ REPRODUCEME.md
@@ -1,7 +1,7 @@
 APOGEE / Hipparcos
 ==================
 
-1. Cross-match APOGEE and Hipparcos (2007) catalogs. This results in `APOGEE-allStar-v603-Hipparcos.fits.gz`
+1. Cross-match APOGEE (v603) and Hipparcos (2007) catalogs. This gives you `data/APOGEE-allStar-v603-Hipparcos.fits.gz`
 
 2. `cd` to the `data/` directory and run `get_apogee_spectra.py` to download the corresponding `apStar-*` spectra.
 
--- data/get_apogee_spectra.py
+++ data/get_apogee_spectra.py
@@ -9,24 +9,48 @@ import os
 from glob import glob
 from astropy.io import fits
 
+# Which spectra should be downloaded, and to where?
+APSTAR, ASPCAP = (True, True)
 APOGEE_DATA_FOLDER = "APOGEE/"
 
 data = fits.open("APOGEE-allStar-v603-Hipparcos.fits.gz")[1].data
-command = ("wget -O {APOGEE_DATA_FOLDER}/HIP{hip}.fits http://data.sdss3.org/"
-    "sas/dr12/apogee/spectro/redux/r5/stars/{telescope}/{field}/{file}")
 
-if not os.path.exists(APOGEE_DATA_FOLDER):
-    os.mkdir(APOGEE_DATA_FOLDER)
+if APSTAR:
 
-before = len(glob("{}/*.fits".format(APOGEE_DATA_FOLDER)))
-for row in data:
-    os.system(command.format(
-        APOGEE_DATA_FOLDER=APOGEE_DATA_FOLDER,
-        telescope=row["TELESCOPE"],
-        file=row["FILE"],
-        field=row["LOCATION_ID"] if row["LOCATION_ID"] > 1 else "hip",
-        hip=row["HIP"]))
-after = len(glob("{}/*.fits".format(APOGEE_DATA_FOLDER)))
+    command = ("wget -O {APOGEE_DATA_FOLDER}/apStar/HIP{hip}.fits"
+        " http://data.sdss3.org/sas/dr12/apogee/spectro/redux/r5/"
+        "stars/{telescope}/{field}/{file}")
 
-print("New files in {0}/: {1} (from {2} rows)".format(APOGEE_DATA_FOLDER,
-    after - before, len(data)))
+    if not os.path.exists("{}/apStar/".format(APOGEE_DATA_FOLDER)):
+        os.mkdir("{}/apStar/".format(APOGEE_DATA_FOLDER))
+
+    for row in data:
+        os.system(command.format(
+            APOGEE_DATA_FOLDER=APOGEE_DATA_FOLDER,
+            telescope=row["TELESCOPE"],
+            file=row["FILE"],
+            field=row["LOCATION_ID"] if row["LOCATION_ID"] > 1 else row["FIELD"],
+            hip=row["HIP"]))
+    
+if ASPCAP:
+
+    # Get the aspCap stuff.
+    command = ("wget -O {APOGEE_DATA_FOLDER}/aspCap/HIP{hip}.fits"
+        " http://data.sdss3.org/sas/dr12/apogee/spectro/redux/r5/"
+        "stars/l25_6d/v603/{field}/{file}")
+
+    if not os.path.exists("{}/aspCap/".format(APOGEE_DATA_FOLDER)):
+        os.mkdir("{}/aspCap/".format(APOGEE_DATA_FOLDER))
+
+    for row in data:
+        if 0 > row["TEFF"]: continue
+
+        # Download the aspCap stuff.
+        os.system(command.format(
+            APOGEE_DATA_FOLDER=APOGEE_DATA_FOLDER,
+            file=row["FILE"].replace("apStar-r5-", "aspcapStar-r5-v603-"),
+            field=row["LOCATION_ID"] if row["LOCATION_ID"] > 1 else row["FIELD"],
+            hip=row["HIP"]))
+
+
+    
