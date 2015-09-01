d87c9e613e0fb84901c0b120efb04e9a3b6a94af
Andy Casey <andycasey@gmail.com>
2015-09-01 12:01:27 +0100

    Scripts to get APOGEE cluster spectra from Ness et al. (2015)

--- data/prepare_data.py
+++ data/prepare_data.py
@@ -20,12 +20,96 @@ __cluster_distances = {
     "M67": (   908, 8.40, "Kharchenko et al. (2005)"),
     "M2":  (11.5e3, 0.32, "Harris"),
     "M3":  (10.2e3, 0.37, "Harris"),
+    "M5":  ( 7.5e3, 0.44, "Harris"),
     "M13": ( 7.1e3, 0.62, "Harris"),
     "M15": (10.4e3, 0.14, "Harris"), # c
     "M53": (17.9e3, 0.35, "Harris"),
     "M71": ( 4.0e3, 0.63, "Harris"),
     "M92": ( 8.3e3, 0.26, "Harris"),
+    "M107": (6.4e3, 0.56, "Harris"),
+    "N188": (2301.442, np.nan, "2010MNRAS.403.1491P"),
+    "N2158": (8472.274, np.nan, "2010MNRAS.403.1491P"),
+    # NGC 2420 distance modulus in V 11.88 ± 0.27
+    # mu = 5 * log10(D) - 5
+    "N2420": (2376.8, np.nan, "http://www.aanda.org/articles/aa/pdf/2010/03/aa12965-09.pdf"),
+    "N4147": (19.3e3, np.nan, "Harris"),
+    "N5466": (16.0e3, np.nan, "Harris"),
+    "N6791": (4000.0, np.nan, "http://www.aanda.org/articles/aa/pdf/2008/46/aa10749-08.pdf"),
+    "N6819": (2910.0, np.nan, "https://iopscience.iop.org/article/10.1088/0004-637X/786/1/10/pdf"), # Mv = 12.32 ± 0.03
+    "N7789": (2337.0, np.nan, "2005A&A...438.1163K")
 }
+
+
+LARGE_VARIANCE = 1e8
+
+
+def get_apogee_ness_cluster_sample(perturb_distances=False):
+    """
+    Prepare APOGEE Cluster spectra from Ness and data table in a convenient format.
+    """
+
+    spectra_filenames = glob("APOGEE/Ness_Clusters/*/*.fits")
+
+    N_stars = len(spectra_filenames)
+    with fits.open(spectra_filenames[0]) as image:
+        N_pixels = image[1].data.size
+        wavelengths = 10**(image[1].header["CRVAL1"] + \
+            np.arange(image[1].data.size) * image[1].header["CDELT1"])
+
+    fluxes = np.zeros((N_stars, N_pixels))
+    flux_uncertainties = np.zeros((N_stars, N_pixels))
+
+    for i, filename in enumerate(spectra_filenames):
+        print("{0}/{1}: {2}".format(i + 1, N_stars, filename))
+
+        with fits.open(filename) as image:
+            fluxes[i, :] = np.atleast_2d(image[1].data)[0, :].copy()
+            flux_uncertainties[i, :] = np.atleast_2d(image[2].data)[0, :].copy()
+
+    # Quality control.
+    zero_pixels_everytime = np.all(0 >= fluxes, axis=0)
+    fluxes[:, zero_pixels_everytime] = 1.0
+    flux_uncertainties[:, zero_pixels_everytime] = LARGE_VARIANCE
+
+    unphysical_uncertainties = (0 >= flux_uncertainties) \
+        + ~np.isfinite(flux_uncertainties)
+    flux_uncertainties[unphysical_uncertainties] = LARGE_VARIANCE
+
+    unphysical_fluxes = (0 >= fluxes) + ~np.isfinite(fluxes)
+    fluxes[unphysical_fluxes] = 1.0
+    flux_uncertainties[unphysical_fluxes] = LARGE_VARIANCE
+
+    # Match each filename to the APOGEE stars.
+    apogee = fits.open("APOGEE-allStar-v603.fits")[1].data
+    indices = np.ones(N_stars, dtype=int)
+    mu = np.zeros(N_stars)
+    for i, filename in enumerate(spectra_filenames):
+        _ = os.path.basename(filename).replace(
+            "aspcapStar-r5-v603-", "apStar-r5-")
+
+        index = np.where(apogee["FILE"] == _)[0]
+        assert len(index) == 1
+        indices[i] = index[0]
+
+        cluster_name = filename.split("/")[-2]
+        #mu = 5 * log_10(d [pc]) - 5
+        distance, core_radius_arcmin = __cluster_distances[cluster_name][:2]
+
+        if perturb_distances:            
+            # Calculate core_radius in parsecs.
+            core_radius = distance * np.tan(np.pi/180 * core_radius_arcmin/60.)
+            distance_realisation = np.random.normal(distance, core_radius)
+
+        else:
+            distance_realisation = distance
+
+        mu[i] = 5. * np.log10(distance_realisation) - 5
+
+    cluster_stars = Table(apogee[indices])
+    cluster_stars["mu"] = mu
+
+    return (cluster_stars, wavelengths, fluxes, flux_uncertainties)
+
 
 
 def get_apogee_cluster_sample(perturb_distances=False):
@@ -53,17 +137,16 @@ def get_apogee_cluster_sample(perturb_distances=False):
 
     # Quality control.
     zero_pixels_everytime = np.all(0 >= fluxes, axis=0)
-    wavelengths = wavelengths[~zero_pixels_everytime]
-    fluxes = fluxes[:, ~zero_pixels_everytime]
-    flux_uncertainties = flux_uncertainties[:, ~zero_pixels_everytime]
+    fluxes[:, zero_pixels_everytime] = 1.0
+    flux_uncertainties[:, zero_pixels_everytime] = LARGE_VARIANCE
 
     unphysical_uncertainties = (0 >= flux_uncertainties) \
         + ~np.isfinite(flux_uncertainties)
-    flux_uncertainties[unphysical_uncertainties] = 1e8
+    flux_uncertainties[unphysical_uncertainties] = LARGE_VARIANCE
 
     unphysical_fluxes = (0 >= fluxes) + ~np.isfinite(fluxes)
     fluxes[unphysical_fluxes] = 1.0
-    flux_uncertainties[unphysical_fluxes] = 1e8
+    flux_uncertainties[unphysical_fluxes] = LARGE_VARIANCE
 
     # Match each filename to the APOGEE stars.
     apogee = fits.open("APOGEE-allStar-v603.fits")[1].data
@@ -85,9 +168,6 @@ def get_apogee_cluster_sample(perturb_distances=False):
             # Calculate core_radius in parsecs.
             core_radius = distance * np.tan(np.pi/180 * core_radius_arcmin/60.)
             distance_realisation = np.random.normal(distance, core_radius)
-
-            if distance > 16000:
-                raise a
 
         else:
             distance_realisation = distance
@@ -135,24 +215,20 @@ def get_apogee_hipparcos_sample():
             flux_uncertainties[i, :] = np.atleast_2d(im[2].data)[0, :].copy()
 
     # Do QC on the arrays:
-    # - zeros in all fluxes --> delete pixel
+    # - zeros in all fluxes --> set with large variance values
     # - zero/negative flux uncertainties --> set to large values.
     # - non-finite or zero fluxes --> set to 1 and set uncertainty as large
-
     zero_pixels_everytime = np.all(0 >= fluxes, axis=0)
-    
-
-    fluxes = fluxes[:, ~zero_pixels_everytime]
-    flux_uncertainties = flux_uncertainties[:, ~zero_pixels_everytime]
-    wavelengths = wavelengths[~zero_pixels_everytime]
+    fluxes[:, zero_pixels_everytime] = LARGE_VARIANCE
+    flux_uncertainties[:, zero_pixels_everytime] = LARGE_VARIANCE
 
     unphysical_uncertainties = (0 >= flux_uncertainties) \
         + ~np.isfinite(flux_uncertainties)
-    flux_uncertainties[unphysical_uncertainties] = 1e8
+    flux_uncertainties[unphysical_uncertainties] = LARGE_VARIANCE
 
     unphysical_fluxes = (0 >= fluxes) + ~np.isfinite(fluxes)
     fluxes[unphysical_fluxes] = 1.0
-    flux_uncertainties[unphysical_fluxes] = 1e8
+    flux_uncertainties[unphysical_fluxes] = LARGE_VARIANCE
 
     # Calculate distance moduli.
     good_stars["mu"] = 5 * np.log10(1000./(good_stars["Plx"])) - 5
@@ -160,7 +236,8 @@ def get_apogee_hipparcos_sample():
     return (good_stars, wavelengths, fluxes, flux_uncertainties)
 
 
-def save_sample(stars, wavelengths, fluxes, flux_uncertainties, prefix, clobber=True):
+def save_sample(prefix, stars, wavelengths, fluxes, flux_uncertainties,
+    clobber=True):
     """
     Save a table and data arrays to disk.
     """
@@ -190,11 +267,11 @@ if __name__ == "__main__":
 
     # Hipparcos (is saaaaah hipstar)
     hipstars, hipwls, hipfluxes, hipflux_uncertainties = get_apogee_hipparcos_sample()
-    save_sample(hipstars, hipwls, hipfluxes, hipflux_uncertainties, "APOGEE-Hipparcos")
+    save_sample("APOGEE-Hipparcos", hipstars, hipwls, hipfluxes, hipflux_uncertainties)
 
-    # Clusters
+    # Clusters (old)
     clstars, clwls, clfluxes, clflux_uncertainties = get_apogee_cluster_sample()
-    save_sample(clstars, clwls, clfluxes, clflux_uncertainties, "APOGEE-Clusters")
+    save_sample("APOGEE-Clusters", clstars, clwls, clfluxes, clflux_uncertainties)
 
     # Both.
     clstars["SAMPLE"] = "CL"
@@ -209,9 +286,33 @@ if __name__ == "__main__":
             _.data[:] = np.nan
             clstars[column] = _
         
+    assert np.allclose(hipwls, clwls)
     both_samples = vstack([hipstars, clstars])
     fluxes = np.vstack([hipfluxes, clfluxes])
     flux_uncertainties = np.vstack([hipflux_uncertainties, clflux_uncertainties])
-    save_sample(both_samples, np.arange(fluxes.shape[1]), fluxes, flux_uncertainties, "APOGEE-Clusters+Hipparcos")
+    save_sample("APOGEE-Clusters+Hipparcos", both_samples, clwls, fluxes, flux_uncertainties)
+
+    # Cluster spectra from Ness et al. (2015)
+    clstars, clwls, clfluxes, clflux_uncertainties = get_apogee_ness_cluster_sample()
+    save_sample("APOGEE-Ness-Clusters", clstars, clwls, clfluxes, clflux_uncertainties)
+
+    # Both w/ Ness
+    clstars["SAMPLE"] = "CL"
+    hipstars["SAMPLE"] = "HIP"
+
+    del hipstars["PARAM_COV"], hipstars["FPARAM_COV"]
+
+    common = set(hipstars.dtype.names).intersection(clstars.dtype.names)
+    for column, (dtype, _) in hipstars.dtype.fields.items():
+        if column not in common:
+            _ = hipstars[column].copy()[:len(clstars)]
+            _.data[:] = np.nan
+            clstars[column] = _
+        
+    assert np.allclose(hipwls, clwls)
+    both_samples = vstack([hipstars, clstars])
+    fluxes = np.vstack([hipfluxes, clfluxes])
+    flux_uncertainties = np.vstack([hipflux_uncertainties, clflux_uncertainties])
+    save_sample("APOGEE-Ness-Clusters+Hipparcos", both_samples, clwls, fluxes, flux_uncertainties)
 
 
